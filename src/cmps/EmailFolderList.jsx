78d2410bf01e0e7ec09e72fa7731f9ed0827328d
Sarit Galanos <saritgalanos@gmail.com>
2023-12-16 17:32:06 +0200

    change sidebar to grid

--- src/cmps/EmailFolderList.jsx
+++ src/cmps/EmailFolderList.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { utilService } from "../services/util.service"
 
 export function EmailFolderList() {
-    
+
     function OnStarFilter() {
 
     }
@@ -11,12 +11,33 @@ export function EmailFolderList() {
 
         <section className="email-folder-list">
             <button className="compose-button">Compose</button>
-            <div className="image-with-text"><img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('inbox', false)} /> <p>Inbox</p> </div>
-            <div className="image-with-text"><img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('star', false)} /> <p>Starred</p> </div>
-            <div className="image-with-text"><img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('sent',false)} /> <p>Sent</p> </div>
-            <div className="image-with-text"><img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('draft',false)} /> <p>Draft</p> </div>
-            <div className="image-with-text"><img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('trash', false)} /> <p>Trash</p> </div>
-        </section>
+            <img className="inbox-folder-icon email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('inbox', false)} />
+            <div> inbox </div>
+            <div> 5 </div>
+            <img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('star', false)} />
+            <div> Starred </div>
+            <div> 9 </div>
+            <img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('sent', false)} />
+            <div>Sent </div>
+            <div> 7 </div>
+            <img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('draft', false)} />
+            <div> Draft </div>
+            <div> 6</div>
+            <img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('trash', false)} />
+            <div> Trash </div>
+            <div> 8 </div>
+
+
+
+        </section >
+
+        //    <button className="compose-button">Compose</button>
+        //     <img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('inbox', false)} /> <div>Inbox </div>
+        //     <img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('star', false)} /><div> Starred </div>
+        //     <img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('sent', false)} /> <div>Sent </div>
+        //     <img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('draft', false)} /><div> Draft </div>
+        //     <img className="email-folder-icon" onClick={() => OnStarFilter()} src={utilService.getIconUrl('trash', false)} /><div> Trash </div> 
+        // </section >
 
     )
 }
