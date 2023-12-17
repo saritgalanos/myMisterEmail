0f3f27bd0f0a607849fe3a99d5fdcb40859ae599
Sarit Galanos <saritgalanos@gmail.com>
2023-12-17 21:12:28 +0200

    send closes modal

--- src/cmps/EmailCompose.jsx
+++ src/cmps/EmailCompose.jsx
@@ -5,19 +5,19 @@ export function EmailCompose({ onClose }) {
     return (
         <div className="Compose">
 
-            <div className="modal-overlay" onClick={onClose}>
+         
                 <div className="modal-content">
-                <div className="compose-header">New Message</div>
+                    <div className="compose-header">New Message</div>
                     <span className="close-btn compose-header" onClick={onClose}>X</span>
-                    <div className='input-field'>from</div> <div></div>
+                    <div className='input-field'>From <span className="internal-text">saritgalanos@gmail.com</span></div> <div></div>
                     <div className='input-field'>To</div> <div></div>
-               <div className='input-field'>Subject</div><div></div>
-               <div className=' input-body'>Compose your email</div><div></div>
-               <div>
-                <button className='send-button'>Send</button>
-               </div>
-               </div>
+                    <div className='input-field'>Subject</div><div></div>
+                    <div className=' input-body'>Compose your email</div><div></div>
+                    <div>
+                        <button className='send-button' onClick={onClose}>Send</button>
+                    </div>
               
+
 
             </div>
         </div>
