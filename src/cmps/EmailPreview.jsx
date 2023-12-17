b5f9aed71183fe8b78909a5869a696c0602cf293
Sarit Galanos <saritgalanos@gmail.com>
2023-12-17 20:13:50 +0200

    fix preview layout

--- src/cmps/EmailPreview.jsx
+++ src/cmps/EmailPreview.jsx
@@ -11,22 +11,24 @@ export function EmailPreview({ email, onStar, onRemoveEmail }) {
         setIsStar(!isStar)
         onStar(email.id)
     }
-     const emailReadClass = email.isRead ? '' : 'email-not-read'
-   
-      return (
+    const emailReadClass = email.isRead ? '' : 'email-not-read'
+
+    return (
 
         <div className="email-preview">
             {/* <label>
                 <input className="preview-checkbox" type="checkbox" value="selected" checked={false} onChange={() => { }} />
             </label> */}
-            <img className="icon preview-star" onClick={() => OnstarPreview()} src={utilService.getIconUrl('star',isStar)} />
+            <img className="icon preview-star" onClick={() => OnstarPreview()} src={utilService.getIconUrl('star', isStar)} />
             <Link className="email-line" to={`${email.id}`}>
                 <div className={`email-preview-from ${emailReadClass}`}>{email.from}</div>
-                <div className={`email-preview-subject ${emailReadClass}`}> {email.subject} </div>
-                <div className="email-preview-body"> {email.body} </div>
+                <div className="main-data">
+                    <span className={`email-preview-subject ${emailReadClass}`}> {email.subject} </span>
+                    <span className="email-preview-body"> {email.body} </span>
+                </div>
                 <div className={`email-preview-sent-at ${emailReadClass}`}>{utilService.getDateToDisplay(new Date(email.sentAt))}</div>
             </Link>
-            <img className="icon preview-trash" onClick={()=> {onRemoveEmail(email.id)}} src={utilService.getIconUrl('trash', false)} />
+            <img className="icon preview-trash" onClick={() => { onRemoveEmail(email.id) }} src={utilService.getIconUrl('trash', false)} />
 
         </div>
     )
