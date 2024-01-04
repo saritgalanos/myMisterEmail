45a529f2d936eee01820e96d7aa46d33535ab7bf
Sarit Galanos <saritgalanos@gmail.com>
2024-01-04 17:08:10 +0200

    initial move from css to scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
-import './assets/css/index.css'
+import './assets/scss/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
